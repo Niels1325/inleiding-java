acf7692713faeb9a3c341ed2c510837b16326fe0
NeLLo <Nkloosterman@roc-dev.com>
2020-09-21 13:12:39 +0200

    Made 11.7

---
+++ src/H11/Opdr11_7.java
@@ -0,0 +1,30 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr11_7 extends Applet {
+
+    public void paint(Graphics g) {
+        g.setFont(new Font("default", Font.BOLD, 12));
+        setBackground(Color.PINK);
+        g.setColor(Color.WHITE);
+
+        int X = 10;
+        int Y = 10;
+        int H = 1000;
+        int B = 1000;
+        int Arcangle = 360;
+        int Arcstart = 0;
+        int Aantal = 0;
+
+        while (Aantal < 50) {
+            Aantal++;
+            H -= 20;
+            B -= 20;
+            X += 10;
+            Y += 10;
+            g.drawArc(X,Y,B,H,Arcstart,Arcangle);
+        }
+    }
+}
