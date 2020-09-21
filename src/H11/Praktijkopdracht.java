b4610cae4210082200f55040e0dfc6a32986cda9
NeLLo <Nkloosterman@roc-dev.com>
2020-09-21 13:36:04 +0200

    Fixed some bugs

--- src/H11/Praktijkopdracht.java
+++ src/H11/Praktijkopdracht.java
@@ -30,16 +30,16 @@ public class Praktijkopdracht extends Applet {
         for (int i=1; i<=10; i++){
             antwoord = A*i;
             g.drawString("" + antwoord,100,i*20+30);
-            g.drawString("1 x " + A + "=",50,50);
-            g.drawString("2 x " + A + "=",50,70);
-            g.drawString("3 x " + A + "=",50,90);
-            g.drawString("4 x " + A + "=",50,110);
-            g.drawString("5 x " + A + "=",50,130);
-            g.drawString("6 x " + A + "=",50,150);
-            g.drawString("7 x " + A + "=",50,170);
-            g.drawString("8 x " + A + "=",50,190);
-            g.drawString("9 x " + A + "=",50,210);
-            g.drawString("10 x " + A + "=",45,230);
+            g.drawString("1 x " + A + " =",50,50);
+            g.drawString("2 x " + A + " =",50,70);
+            g.drawString("3 x " + A + " =",50,90);
+            g.drawString("4 x " + A + " =",50,110);
+            g.drawString("5 x " + A + " =",50,130);
+            g.drawString("6 x " + A + " =",50,150);
+            g.drawString("7 x " + A + " =",50,170);
+            g.drawString("8 x " + A + " =",50,190);
+            g.drawString("9 x " + A + " =",50,210);
+            g.drawString("10 x " + A + " =",45,230);
         }
     }
 
