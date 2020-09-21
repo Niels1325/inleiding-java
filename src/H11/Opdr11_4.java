d6bf89bee5567b45dbdbd2ad0f459268e4fed5c6
NeLLo <Nkloosterman@roc-dev.com>
2020-09-21 12:52:42 +0200

    Made 11.4

---
+++ src/H11/Opdr11_4.java
@@ -0,0 +1,25 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr11_4 extends Applet {
+
+    public void paint(Graphics g) {
+        setBackground(Color.pink);
+        int breedte = 10;
+        int getal;
+        int cijfer = 0;
+        int x = 10;
+        while(cijfer < 10) {
+            x += 15;
+            cijfer++;
+            getal = 3 * cijfer;
+            g.drawString("" + getal,x,breedte);
+            breedte = breedte + 20;
+        }
+
+
+    }
+
+}
