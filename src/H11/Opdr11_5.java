a6ba013e1bfec120249f26087264d1cade6a1db3
NeLLo <Nkloosterman@roc-dev.com>
2020-09-21 13:07:27 +0200

    Made 11.5

---
+++ src/H11/Opdr11_5.java
@@ -0,0 +1,25 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+public class Opdr11_5 extends Applet {
+
+    public void paint(Graphics g) {
+        g.setFont(new Font("default", Font.BOLD, 12));
+        setBackground(Color.PINK);
+        g.setColor(Color.WHITE);
+        int Aantal = 0;
+        int X = 10;
+        int Y = 10;
+        int B = 40;
+        int H = 40;
+
+        while (Aantal < 5) {
+            Aantal++;
+            X += 40;
+            Y += 40;
+            g.drawRect(X,Y,B,H);
+        }
+    }
+}
