7b598815fa98394863e0842c1d64df761ed6e983
NeLLo <Nkloosterman@roc-dev.com>
2020-09-21 13:01:49 +0200

    Fixed 11.2

--- src/H11/Opdr11_2.java
+++ src/H11/Opdr11_2.java
@@ -10,8 +10,7 @@ public class Opdr11_2 extends Applet {
     public void init() {
 
     getal = 10;
-    breedte = 20;
-    eind = 20;
+    eind = 19;
 
     }
 
@@ -20,11 +19,8 @@ public class Opdr11_2 extends Applet {
         g.setFont(new Font("default", Font.BOLD, 12));
         setBackground(Color.PINK);
         g.setColor(Color.WHITE);
-        for(int i = 10; i < eind; i++) {
-
-            g.drawString("" + getal, breedte, 60 );
-            getal = getal + 1;
-            breedte = breedte + 20;
+        for(int i = getal; i <= eind; i++) {
+            g.drawString("" + i,i*15,20);
         }
 
     }
