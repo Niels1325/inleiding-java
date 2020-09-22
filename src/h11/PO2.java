4f585cda6663ad7795950dd6c15e2d90426910da
NeLLo <Nkloosterman@roc-dev.com>
2020-09-22 14:59:33 +0200

    renamed an initial

--- src/h11/PO2.java
+++ src/h11/PO2.java
@@ -6,14 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class PO2 extends Applet {
-    int A;
+    int factor;
     long antwoord;
     Button knop;
     int i;
 
-
     public void init() {
-        A=0;
+        factor =0;
         antwoord=0;
         knop = new Button("OK");
         knop.addActionListener(new Submit());
@@ -26,26 +25,26 @@ public class PO2 extends Applet {
         g.setColor(Color.WHITE);
 
         for (i=1; i<=10; i++){
-            antwoord = A*i;
+            antwoord = factor *i;
             g.drawString("" + antwoord,100,i*20+30);
-            g.drawString("1 x " + A + " =",50,50);
-            g.drawString("2 x " + A + " =",50,70);
-            g.drawString("3 x " + A + " =",50,90);
-            g.drawString("4 x " + A + " =",50,110);
-            g.drawString("5 x " + A + " =",50,130);
-            g.drawString("6 x " + A + " =",50,150);
-            g.drawString("7 x " + A + " =",50,170);
-            g.drawString("8 x " + A + " =",50,190);
-            g.drawString("9 x " + A + " =",50,210);
-            g.drawString("10 x " + A + " =",45,230);
+            g.drawString("1 x " + factor + " =",50,50);
+            g.drawString("2 x " + factor + " =",50,70);
+            g.drawString("3 x " + factor + " =",50,90);
+            g.drawString("4 x " + factor + " =",50,110);
+            g.drawString("5 x " + factor + " =",50,130);
+            g.drawString("6 x " + factor + " =",50,150);
+            g.drawString("7 x " + factor + " =",50,170);
+            g.drawString("8 x " + factor + " =",50,190);
+            g.drawString("9 x " + factor + " =",50,210);
+            g.drawString("10 x " + factor + " =",45,230);
         }
     }
 
     private class Submit implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            i= Integer.parseInt(String.valueOf(A++));
-            if ( A > 10 ) {
-                A=1;
+            i = Integer.parseInt(String.valueOf(factor++));
+            if ( factor > 10 ) {
+                factor =1;
             }
             repaint();
 
