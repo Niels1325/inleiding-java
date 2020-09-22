44e446abca08ca950aa6581f077ad171c662940c
NeLLo <Nkloosterman@roc-dev.com>
2020-09-22 13:01:37 +0200

    fixed a couple of bugs

--- src/h12/Opdr12_2.java
+++ src/h12/Opdr12_2.java
@@ -6,10 +6,6 @@ import java.awt.*;
 public class Opdr12_2 extends Applet {
 
     public void paint(Graphics g) {
-        g.setFont(new Font("default", Font.BOLD, 12));
-        setBackground(Color.PINK);
-        g.setColor(Color.WHITE);
-
         for(int i = 0; i <= 24; i++) {
             Button knop;
             knop = new Button("Knop");
