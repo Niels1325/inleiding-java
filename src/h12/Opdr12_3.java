60d2efe07928cd1fa1eb22cb21f695f8e580a960
NeLLo <Nkloosterman@roc-dev.com>
2020-10-06 11:24:46 +0200

    fixed some backgrounds

--- src/h12/Opdr12_3.java
+++ src/h12/Opdr12_3.java
@@ -28,7 +28,7 @@ public class Opdr12_3 extends Applet {
 
     public void paint(Graphics g) {
         g.setFont(new Font("default", Font.BOLD, 12));
-        setBackground(Color.PINK);
+        setBackground(Color.black);
         g.setColor(Color.WHITE);
 
     }
