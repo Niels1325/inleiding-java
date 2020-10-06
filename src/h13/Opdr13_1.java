0b7c2960b0ce8797f607b94abc33947155266376
NeLLo <Nkloosterman@roc-dev.com>
2020-10-06 11:21:35 +0200

    Made 13.1

--- src/h13/Opdr13_1.java
+++ src/h13/Opdr13_1.java
@@ -13,9 +13,10 @@ public class Opdr13_1 extends Applet {
     }
 
     void drawTriangle(Graphics g, int x1, int y1, int x2, int y2, int x3, int y3) {
-        g.drawLine(x1, y1, x2, y2);
+
         g.drawLine(x2, y2, x3, y3);
         g.drawLine(x3, y3, x1, y1);
+        g.drawLine(x1, y1, x2, y2);
 
     }
 }
